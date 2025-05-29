12bb3f0cbeaa2a82937318f51003fc22a82430cc
Poon Yip Hoon (Aeric) <yiphoon@yahoo.com>
2025-05-29 21:52:12 +0800

    add release directory

---
+++ release/Objects/www/css/main.css
@@ -0,0 +1,5 @@
+/*!
+ * Inti v1.00 by @pyhoon (https://github.com/pyhoon/inti-b4j)
+ * Copyright (c) 2022-2025 Poon Yip Hoon (Aeric)
+ * Licensed under MIT (https://github.com/pyhoon/inti-b4j/blob/main/LICENSE)
+ */
